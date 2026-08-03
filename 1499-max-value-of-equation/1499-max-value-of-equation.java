632088697b56fa9435d9e9964f340a2a6e815de2
Vikas B M <vbm3546@gmail.com>
2026-08-03 16:41:22 +0530

    Time: 7 ms (100%), Space: 113.6 MB (84.25%) - LeetHub

---
+++ 1499-max-value-of-equation/1499-max-value-of-equation.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findMaxValueOfEquation(int[][] points, int k) {
+        int ans = Integer.MIN_VALUE;
+        int left = 0;
+        int right = 1;
+
+        while (left < points.length) {
+            if (right < left + 1) right = left + 1;
+            for (int j = right; j <= points.length - 1; j++) {
+                if (points[j][0] > (points[left][0] + k))
+                    break;
+
+                int sum = points[left][1] + points[j][1] + points[j][0] - points[left][0];
+                if (sum > ans) {
+                    ans = Math.max(ans, sum);
+                    right = j - 1;
+                }
+            }
+            left++;
+        }
+        return ans;
+    }
+}
