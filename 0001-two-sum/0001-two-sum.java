9e63be081eccd5273487a32676e6136637fd6797
Vikas B M <vbm3546@gmail.com>
2026-08-03 16:10:43 +0530

    Time: 45 ms (26.63%), Space: 47.1 MB (59.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,5 +1,6 @@
 class Solution {
-    static int[] twoSum(int[] arr,int target){
+    
+    public  int[] twoSum(int[] arr,int target){
         int[] twoSum=new int[2];
         for(int i=0;i<arr.length-1;i++){
             for(int j=i+1;j<arr.length;j++){
