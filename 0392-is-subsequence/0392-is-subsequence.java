5bbd396f79435958873d9e16451bf79646f19175
Vikas B M <vbm3546@gmail.com>
2026-08-11 22:48:46 +0530

    Time: 0 ms (100%), Space: 42.8 MB (63.11%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        for(int i=0;i<s.length();i++){
+            if(t.indexOf(s.charAt(i)) == -1){
+                return false;
+            }
+            t = t.substring(t.indexOf(s.charAt(i))+1);
+        }
+        return true;
+    }
+}
